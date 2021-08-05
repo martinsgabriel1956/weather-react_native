713f0571c079d22573d9bf8fbf50a33e94de8061
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-08-05 17:58:22 -0300

    feat: still constructing

--- App.tsx
+++ App.tsx
@@ -1,8 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [errorMsg, setErrorMsg] = useState<string | null>();
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  async function load() {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') setErrorMsg('Access to location is needed to run the app!')
+
+      const location = await Location.getCurrentPositionAsync();
+
+      const {latitude, longitude} = location.coords;
+
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`)
+    } catch (e) {
+
+    }
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
